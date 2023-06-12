ad4cf18c0ca29c9675a4296c03af55d2dac1f699
DAVS <dvidelasilva@gmail.com>
2023-06-11 22:43:02 -0400

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Programa Calculo Perímetro de Circunferencia
+ * @Autor: Diego Videla Silva
+/*
+ *Este programa escribe el texto "Hola Mundo" en la consola
+ * utilizando el metodo System.out.println()
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo - DAVS");
+    }
+}
